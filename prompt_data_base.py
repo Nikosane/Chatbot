46a2fe152bebcd8e7b9b6bb86cf810adf8aba5ff
Nikosane <linux.nikosane@gmail.com>
2024-01-08 22:54:43 +0530

    data base information

--- prompt_data_base.py
+++ prompt_data_base.py
@@ -1,17 +1,17 @@
-# import pypyodbc as odbc
-#
-# DRIVER_NAME = 'SQL Server'
-# SERVER_NAME = 'DESKTOP-UU6IQ1P\\SQLEXPRESS'
-# DATABASE_NAME = 'prompt_data_base'
-#
-# connection_string = f"""
-#     DRIVER={{{DRIVER_NAME}}};
-#     SERVER={SERVER_NAME};
-#     DATABASE={DATABASE_NAME};
-#     Trusted_Connection=yes;
-# """
-#
-# conn = odbc.connect(connection_string)
-# cursor = conn.cursor()
+import pypyodbc as odbc
+
+DRIVER_NAME = 'SQL Server'
+SERVER_NAME = 'DESKTOP-UU6IQ1P\\SQLEXPRESS'
+DATABASE_NAME = 'prompt_data_base'
+
+connection_string = f"""
+    DRIVER={{{DRIVER_NAME}}};
+    SERVER={SERVER_NAME};
+    DATABASE={DATABASE_NAME};
+    Trusted_Connection=yes;
+"""
+
+conn = odbc.connect(connection_string)
+cursor = conn.cursor()
 
 
